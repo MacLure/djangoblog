2a6e7de29210695802c2e3a12c77bbe42c9a8fb0
Malcolm <malcolm.maclure@gmail.com>
2019-04-24 20:21:21 -0400

    posts display in home page

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-posts = [
-    {'author': 'author1',
-     'title': 'title1',
-     'content': 'body1',
-     'date_posted': 'date'},
-    {'author': 'author2',
-     'title': 'title2',
-     'content': 'body2',
-     'date_posted': 'date2'}, ]
 
 
 def home(request):
